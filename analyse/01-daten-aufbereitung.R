caf56c74ff05bd5ee914573bc1be4b34a04af798
hugobre <hugo.bretscher@zh.ch>
2024-12-03 09:53:47 +0000

    Projektaktualisierung

---
+++ analyse/01-daten-aufbereitung.R
@@ -0,0 +1,15 @@
+# R-Pakete laden ----------------------------------------------------------
+
+library(tidyverse)
+library(ggthemes)
+
+# Daten importieren -------------------------------------------------------
+winterthur <- read_csv(here::here("daten/raw/KTZH_00002604_00005328.csv"))
+
+# Daten bereinigen
+winterthur_daten_bereinigt <- winterthur |> 
+  filter(stadtkreis %in% c("Stadt","Veltheim","Seen", "Töss", "Mattenbach", "Oberwinterthur", "Wülflingen"))
+
+# Daten speichern
+write_csv(winterthur_daten_bereinigt,
+          here::here("daten/processed/Daten-Projekt-hugobre-bereinigt.csv"))
